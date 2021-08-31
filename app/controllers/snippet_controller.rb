2b54ec4f63781574685270a330c508f3cfafdb6c
Clivern <hello@clivern.com>
2021-08-31 23:55:34 +0200

    update

--- app/controllers/snippet_controller.rb
+++ app/controllers/snippet_controller.rb
@@ -27,28 +27,28 @@ class SnippetController < ApiController
     req = ActiveSupport::JSON.decode(request.body.read)
 
     # Create a unique snippet slug
-		loop do
-		  @slug = SecureRandom.hex
-		  if !Snippet.find_by_slug(@slug)
-		    break
-		  end
-		end
+    loop do
+      @slug = SecureRandom.hex
+      if !Snippet.find_by_slug(@slug)
+        break
+      end
+    end
 
-  	@snippet = Snippet.create(
-    	slug: @slug,
-    	language: req["language"] == nil ? '' : req["language"],
-    	title: req["title"] == nil ? '' : req["title"],
-    	code: req["code"] == nil ? '' : req["code"],
-    	password: req["password"] == nil ? '' : req["password"],
-    	is_public: req["is_public"] == nil ? true : req["is_public"],
-    	created_at: DateTime.now,
-    	updated_at: DateTime.now
-  	)
+    @snippet = Snippet.create(
+      slug: @slug,
+      language: req["language"] == nil ? '' : req["language"],
+      title: req["title"] == nil ? '' : req["title"],
+      code: req["code"] == nil ? '' : req["code"],
+      password: req["password"] == nil ? '' : req["password"],
+      is_public: req["is_public"] == nil ? true : req["is_public"],
+      created_at: DateTime.now,
+      updated_at: DateTime.now
+    )
 
-		if @snippet
-	  	render json: @snippet, status: 201
-		else
-	  	render json: {'errorMessage': 'Unable to create a new snippet'}, status: 500
-		end
+    if @snippet
+      render json: @snippet, status: 201
+    else
+      render json: {'errorMessage': 'Unable to create a new snippet'}, status: 500
+    end
   end
 end
